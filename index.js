0623cd0d0910a7b136624c6dba14ee07d7cda9ef
ming lee <hi@waphe.com>
2018-04-27 17:13:37 +0800

    export constants.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 module.exports = {
-	crypto : require("./lib/transactions/crypto.js"),
+	crypto: require("./lib/transactions/crypto.js"),
 	dapp: require("./lib/transactions/dapp.js"),
 	transfer: require("./lib/transactions/transfer.js"),
-	delegate : require("./lib/transactions/delegate.js"),
-	signature : require("./lib/transactions/signature.js"),
-	transaction : require("./lib/transactions/transaction.js"),
-	vote : require("./lib/transactions/vote.js"),
+	delegate: require("./lib/transactions/delegate.js"),
+	signature: require("./lib/transactions/signature.js"),
+	transaction: require("./lib/transactions/transaction.js"),
+	vote: require("./lib/transactions/vote.js"),
 	uia: require("./lib/transactions/uia.js"),
 	storage: require("./lib/transactions/storage.js"),
 	options: require("./lib/options.js"),
+	constants: require("./lib/constants"),
 	utils: {
 		slots: require("./lib/time/slots.js"),
 		format: require("./lib/time/format.js")
